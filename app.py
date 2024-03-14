3e717d34da94e43ee17d7b5635165e74d41acb50
Username <tianjian@sdataft.com>
2024-03-14 11:21:51 +0800

    aa

--- app.py
+++ app.py
@@ -5,6 +5,46 @@ import json
 import requests
 
 
+from langchain.llms.base import LLM
+from typing import Any, List, Optional
+from langchain.callbacks.manager import CallbackManagerForLLMRun
+from transformers import AutoTokenizer, AutoModelForCausalLM
+import torch
+# from langchain.prompts import PromptTemplate
+# from langchain.chains import RetrievalQA
+
+class InternLM_LLM(LLM):
+    # 基于本地 InternLM 自定义 LLM 类
+    tokenizer: AutoTokenizer = None
+    model: AutoModelForCausalLM = None
+
+    def __init__(self, model_path: str):
+        # model_path: InternLM 模型路径
+        # 从本地初始化模型
+        super().__init__()
+        print("正在从本地加载模型...")
+        self.tokenizer = AutoTokenizer.from_pretrained(model_path, trust_remote_code=True)
+        self.model = AutoModelForCausalLM.from_pretrained(model_path, trust_remote_code=True).to(torch.bfloat16).cuda()
+        self.model = self.model.eval()
+        print("完成本地模型的加载")
+
+    def _call(self, prompt: str, stop: Optional[List[str]] = None,
+              run_manager: Optional[CallbackManagerForLLMRun] = None,
+              **kwargs: Any):
+        # 重写调用函数
+        system_prompt = """你名叫萤萤，是一个陪伴型聊天机器人，负责安慰，鼓励，以及心理治愈。
+        - YingYing is a conversational language model that is developed by SSJT (上海数据集团). It is designed to be helpful, honest, and harmless.
+        - YingYing (萤萤) can understand and communicate fluently in the language chosen by the user such as English and 中文.
+        """
+
+        messages = [(system_prompt, '')]
+        response, history = self.model.chat(self.tokenizer, prompt, history=messages)
+        return response
+
+    @property
+    def _llm_type(self) -> str:
+        return "InternLM"
+
 def call_model(prompt):
     token = os.getenv('WToken')
     url = 'https://aip.baidubce.com/rpc/2.0/ai_custom/v1/wenxinworkshop/chat/ernie-lite-8k'
@@ -24,7 +64,7 @@ def call_model(prompt):
     return result
 
 
-def chat(user_input, history=[]):
+def wenxin_chat(user_input, history=[]):
     current_line = {'role': 'user', 'content': user_input}
     if len(history) == 0:
         prompt = [current_line]
@@ -37,6 +77,21 @@ def chat(user_input, history=[]):
     return response, history
 
 
+def load_InternLM_chain():
+    # 切分文件
+
+    # 加载自定义 LLM
+    llm = InternLM_LLM(model_path="/home/xlab-app-center/internlm2-chat-7b")
+
+    # 定义一个 Prompt Template
+    template = """使用以下上下文来回答最后的问题。如果你不知道答案，就说你不知道，不要试图编造答
+    案。尽量使答案简明扼要。总是在回答的最后说“谢谢你的提问！”。
+    {context}
+    问题: {question}
+    有用的回答:"""
+
+    return llm.predict
+
 class Model_center():
     """
     存储检索问答链的对象
@@ -47,7 +102,7 @@ class Model_center():
         # self.chain = load_chain()
         pass
 
-    def qa_chain_self_answer(self, question: str, chat_history: list = []):
+    def qa_chain_self_answer_demo(self, question: str, chat_history: list = []):
         """
         调用问答链进行回答
         """
@@ -71,7 +126,7 @@ class Model_center():
         if question == None or len(question) < 1:
             return "", chat_history
         try:
-            response, wenxin_history = chat(question, wenxin_history)
+            response, wenxin_history = wenxin_chat(question, wenxin_history)
             chat_history.append(
                 (question, response))
             # 将问答结果直接附加到问答历史中，Gradio 会将其展示出来
@@ -80,14 +135,34 @@ class Model_center():
             return e, chat_history
 
 
-# def download_model():
-#     from openxlab.model import download
-#     download(model_repo='DD-learning/model_demo',
-#              model_name='model_demo', output='/home/xlab-app-center')
-#     print(os.listdir('.'))
-#
-#
-# download_model()
+    def qa_chain_self_answer_interlm(self, question: str, chat_history: list = []):
+        """
+        调用问答链进行回答
+        """
+        internlm_predict = load_InternLM_chain()
+        wenxin_history = []
+        if question == None or len(question) < 1:
+            return "", chat_history
+        try:
+            response = internlm_predict(question)
+            chat_history.append(
+                (question, response))
+            # 将问答结果直接附加到问答历史中，Gradio 会将其展示出来
+            return "", chat_history
+        except Exception as e:
+            return e, chat_history
+
+def download_model():
+    from openxlab.model import download
+    download(model_repo='DD-learning/model_demo',
+             model_name='model_demo', output='/home/xlab-app-center')
+    print(os.listdir('.'))
+
+
+download_model()
+
+
+
 # 实例化核心功能对象
 model_center = Model_center()
 # 创建一个 Web 界面
@@ -116,7 +191,7 @@ with block as demo:
                     components=[chatbot], value="Clear console")
 
         # 设置按钮的点击事件。当点击时，调用上面定义的 qa_chain_self_answer 函数，并传入用户的消息和聊天历史记录，然后更新文本框和聊天机器人组件。
-        db_wo_his_btn.click(model_center.qa_chain_self_answer_wenxin, inputs=[
+        db_wo_his_btn.click(model_center.qa_chain_self_answer_demo, inputs=[
             msg, chatbot], outputs=[msg, chatbot])
 
     gr.Markdown("""提醒：<br>
